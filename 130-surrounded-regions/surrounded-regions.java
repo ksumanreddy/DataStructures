0b60eb2d9e76d23f1039e6bcff6b45e593362689
Suman K <43966391+ksumanreddy@users.noreply.github.com>
2025-04-14 22:27:48 +0530

    Time: 4 ms (22.19%) | Memory: 45.9 MB (22.15%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -10,20 +10,15 @@ class Solution {
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
-        char[][] tempBoard = new char[n+2][m+2];
-        int tn =  tempBoard.length;
-        int tm = tempBoard[0].length;
-        for(int i=0;i<tn;i++){
-            for(int j=0;j<tm;j++){
-                if(i==0 || j==0 || i== tn-1|| j== tm-1){
-                    tempBoard[i][j]='O';
-                }else{
-                    tempBoard[i][j]=board[i-1][j-1];
-                } 
+        Queue<Pair> qu = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+               if((i==0 || j==0 || i==n-1||j==m-1)&& (board[i][j]=='O')){
+                  qu.add(new Pair(i,j));
+                  board[i][j]='V';
+               }
             }
         }
-        Queue<Pair> qu = new LinkedList<>();
-        qu.add(new Pair(0,0));
         while(!qu.isEmpty()){
            Pair p = qu.remove();
            int[] dr = {-1,0,1,0};
@@ -31,16 +26,19 @@ class Solution {
            for(int i=0;i<4;i++){
               int nr = p.row+dr[i];
               int nc = p.col+dc[i];
-              if((nr>=0 && nr<tn) && (nc>=0 && nc<tm) && tempBoard[nr][nc]=='O'){
-                 tempBoard[nr][nc]='V';
+              if((nr>=0 && nr<n) && (nc>=0 && nc<m) && board[nr][nc]=='O'){
+                 board[nr][nc]='V';
                  qu.add(new Pair(nr,nc));
                }
            }
         }
-        for(int i=1;i<tn-2;i++){
-            for(int j=1;j<tm-2;j++){
-                if(tempBoard[i][j]=='O'){
-                   board[i-1][j-1]='X';
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O'){
+                   board[i][j]='X';
+                }
+                if(board[i][j]=='V'){
+                   board[i][j]='O';
                 }
             }
         }
